bacb1d24cf794a4c27809b14691fa42f013c7919
maxniederer <66148024+maxniederer@users.noreply.github.com>
2024-10-10 12:49:54 -0400

    adding mobile images to mobile view

--- slider.js
+++ slider.js
@@ -25,3 +25,22 @@ function updateSlider() {
 function strictMod(i, max) {
   return ((i % max) + max) % max;
 }
+
+function showMobileImages(x) {
+  if (x.matches) {
+    sliderImg[0].src = "images/mobile-image-hero-1.jpg";
+    sliderImg[1].src = "images/mobile-image-hero-2.jpg";
+    sliderImg[2].src = "images/mobile-image-hero-3.jpg";
+  } else {
+    sliderImg[0].src = "images/desktop-image-hero-1.jpg";
+    sliderImg[1].src = "images/desktop-image-hero-2.jpg";
+    sliderImg[2].src = "images/desktop-image-hero-3.jpg";
+  }
+}
+
+var mobileMediaQuery = window.matchMedia("(max-width: 37.5em)");
+
+mobileMediaQuery.addEventListener("change", () => {
+  showMobileImages(mobileMediaQuery);
+});
+showMobileImages(mobileMediaQuery); // execute on load as well
